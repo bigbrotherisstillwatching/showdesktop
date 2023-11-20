ee7c4aacae734cfbb5a19af64c3c075be14b2139
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-11-20 18:51:18 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ int main(int argc, char *argv[])
     view->setSource(QUrl("qml/Main.qml"));
     view->setResizeMode(QQuickView::SizeRootObjectToView);
     view->show();
-    view->lower();
 
     return app->exec();
 }
